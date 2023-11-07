b7b11cd4c93ab4a3333f21acf6b9507125e26e77
Dnunez <neider5135@gmail.com>
2023-11-06 23:55:24 -0500

    Avance introducción a javascript

--- Introducción/main.js
+++ Introducción/main.js
@@ -1,28 +1,106 @@
-const miNombre = prompt("¿Cómo te llamas?")
-        const miApellido = prompt("¿Cuál es tu apellido?")
-        let miEdad = prompt("¿Cuántos años tienes?")
-        const miMascota = prompt("¿Cómo se llama tu mascota?")
-        let edadMascota = prompt("¿Cuántos años tiene tu mascota?")
-        const nombreCompleto = miNombre +" "+ miApellido
-        const textoPresentacion = "Mi nombre es "+nombreCompleto+" tengo "+miEdad+" años y tengo una mascota llamada "+miMascota+" que tiene "+edadMascota+" años"
-        let sumaEdades = miEdad+edadMascota
-        let restaEdades = miEdad-edadMascota
-        let productoEdades = miEdad%edadMascota
-        let divisionEdades = miEdad/edadMascota
-        alert (textoPresentacion)
-        let Alumno = {
-        nombre : "Neider",
-        apellido : "Núñez",
-        edad : 23,
-        mascota : "Paprika",
-        edadMascota : 5,
-    }
-        let Mascota = {
-        nombre : "Paprika",
-        raza : "Criollo",
-        edad : 5,
-        dueño : "Paprika",
-        edadDueño : 23,
-    }
-        let frutas = [ "banano", "manzana", "piña", "mango", "sandía"]
-        let números = [1, 2, 3, 4, 5]
+//1
+let miNombre
+//2
+let miApellido
+//3
+let miEdad
+//4
+let miMascota
+//5
+let edadMascota
+//6
+console.log(miNombre, miApellido, miEdad, miMascota, edadMascota)
+//7
+const nombreCompleto = miNombre + " " + miApellido;
+//8
+const textoPresentacion =
+  "Mi nombre es " +
+  nombreCompleto +
+  " tengo " +
+  miEdad +
+  " años y tengo una mascota llamada " +
+  miMascota +
+  " que tiene " +
+  edadMascota +
+  " años";
+//9
+let sumaEdades = miEdad + edadMascota;
+let restaEdades = miEdad - edadMascota;
+let productoEdades = miEdad % edadMascota;
+let divisionEdades = miEdad / edadMascota;
+
+/*
+//10
+miNombre = prompt("¿Cómo te llamas?");
+miApellido = prompt("¿Cuál es tu apellido?");
+miEdad = prompt("¿Cuántos años tienes?");
+miMascota = prompt("¿Cómo se llama tu mascota?");
+edadMascota = prompt("¿Cuántos años tiene tu mascota?");
+console.log(miNombre, miApellido, miEdad, miMascota, edadMascota)
+nombreCompleto = miNombre + " " + miApellido;
+textoPresentacion =
+  "Mi nombre es " +
+  nombreCompleto +
+  " tengo " +
+  miEdad +
+  " años y tengo una mascota llamada " +
+  miMascota +
+  " que tiene " +
+  edadMascota +
+  " años";
+sumaEdades = miEdad + edadMascota;
+restaEdades = miEdad - edadMascota;
+productoEdades = miEdad % edadMascota;
+divisionEdades = miEdad / edadMascota;
+
+//11
+let Alumno = {
+  nombre: "Neider",
+  apellido: "Núñez",
+  edad: 23,
+  mascota: "Paprika",
+  edadMascota: 5,
+};
+console.table(Alumno)
+console.table(Alumno.nombre)
+console.table(Alumno.apellido)
+console.table(Alumno.edad)
+console.table(Alumno.mascota)
+console.table(Alumno.edadMascota)
+
+//12
+let Mascota = {
+  nombre: "Paprika",
+  raza: "Criollo",
+  edad: 10,
+  dueño: "Neider",
+  edadDueño: 23,
+};
+console.table(Mascota)
+console.table(Mascota.nombre)
+console.table(Mascota.raza)
+console.table(Mascota.edad)
+console.table(Mascota.dueño)
+console.table(Mascota.edadDueño)
+
+//13
+let frutas = ["banano", "manzana", "piña", "mango", "sandía"];
+console.log(frutas)
+console.log(frutas[0])
+console.log(frutas[1])
+console.log(frutas[2])
+console.log(frutas[3])
+console.log(frutas[4])
+
+//14
+
+let números = [1, 2, 3, 4, 5];
+console.log (números)
+console.log (números[0])
+console.log (números[1])
+console.log (números[2])
+console.log (números[3])
+console.log (números[4])
+*/
+
+//15
