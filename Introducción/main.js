b682bfcbce40a5a106223aadf5990bce28b870c3
Dnunez <neider5135@gmail.com>
2023-11-07 11:33:10 -0500

    Introducción a javascript Terminado

--- Introducción/main.js
+++ Introducción/main.js
@@ -1,3 +1,5 @@
+/*
+
 //1
 let miNombre
 //2
@@ -23,13 +25,19 @@ const textoPresentacion =
   " que tiene " +
   edadMascota +
   " años";
+
+  */
+  /*
+
 //9
 let sumaEdades = miEdad + edadMascota;
 let restaEdades = miEdad - edadMascota;
 let productoEdades = miEdad % edadMascota;
 let divisionEdades = miEdad / edadMascota;
 
+*/
 /*
+
 //10
 miNombre = prompt("¿Cómo te llamas?");
 miApellido = prompt("¿Cuál es tu apellido?");
@@ -53,6 +61,9 @@ restaEdades = miEdad - edadMascota;
 productoEdades = miEdad % edadMascota;
 divisionEdades = miEdad / edadMascota;
 
+*/
+/*
+
 //11
 let Alumno = {
   nombre: "Neider",
@@ -68,6 +79,9 @@ console.table(Alumno.edad)
 console.table(Alumno.mascota)
 console.table(Alumno.edadMascota)
 
+*/
+/*
+
 //12
 let Mascota = {
   nombre: "Paprika",
@@ -82,6 +96,8 @@ console.table(Mascota.raza)
 console.table(Mascota.edad)
 console.table(Mascota.dueño)
 console.table(Mascota.edadDueño)
+*/
+/*
 
 //13
 let frutas = ["banano", "manzana", "piña", "mango", "sandía"];
@@ -101,6 +117,102 @@ console.log (números[1])
 console.log (números[2])
 console.log (números[3])
 console.log (números[4])
-*/
+
 
 //15
+
+let familia = [
+  persona1 = {
+    nombre: "Juanito",
+    apellido: "Perez",
+    edad: 50,
+    papelFamiliar: "Padre",
+  },
+  persona2 = {
+    nombre: "Juanita",
+    apellido: "De Perez",
+    edad: 47,
+    papelFamiliar: "Madre",
+  },
+  persona3 = {
+    nombre: "Juanito. Jr",
+    apellido: "Perez",
+    edad: 21,
+    papelFamiliar: "Hijo menor"
+  },
+  persona4 = {
+    nombre: "Juanita",
+    apellido: "Perez",
+    edad: 23,
+    papelFamiliar: "Hija del medio"
+  },
+  persona5 = {
+    nombre: "Juancho",
+    apellido: "Perez",
+    edad: 25,
+    papelFamiliar: "Hijo mayor"
+  },
+]
+
+console.table (familia)
+console.table (familia[0])
+console.table (familia[1])
+console.table (familia[2])
+console.table (familia[3])
+console.table (familia[4])
+
+
+//16
+
+let textoAleatorio = "Yo tenía 1 " + frutas[1] + " pero ahora hay " + números[3] + " frutas más así que le preguntaré a " + familia[0].nombre
+
+console.log (textoAleatorio)
+
+*/
+/*
+
+//17
+
+let num01 = parseInt(prompt("Ingrese su edad"))
+let num02 = parseInt(prompt("Ingrese la edad de su compañero"))
+
+let soyMayor = num01>num02
+let soyMenor =  num01<num02
+let soyIgual = num01==num02
+
+console.log ("a. Mi edad es igual a la de mi compañero:" + soyIgual)
+console.log ("b. Mi edad es mayor a la de mi compañero:" + soyMayor)
+console.log ("c. Mi edad es menor a la de mi compañero:" + soyMenor)
+
+*/
+/*
+//18
+
+let soyMayorDeEdad = parseInt(prompt("Ingrese su edad")) >= 18
+
+console.log("Soy mayor de edad " + soyMayorDeEdad)
+
+*/
+/*
+
+//19
+
+let edadSubir = parseInt(prompt("Ingrese su edad"))
+let alturaSubir = parseInt(prompt("Ingrese su altura"))
+
+let operacion = edadSubir > 6 && alturaSubir >= 120
+
+console.log("Puede subir a la atracción: "+ operacion)
+
+*/
+/*
+
+//20
+
+let tipoEntrada = prompt("Ingrese el tipo de entrada que posee (vip, normal o limitada)").toLocaleLowerCase().trim()
+let saldo = parseInt(prompt("Ingrese el saldo que posee"))
+
+let operacionEntrada = tipoEntrada == "vip" || saldo > 1000
+
+console.log("La persona puede pasar: "+ operacionEntrada)
+*/
